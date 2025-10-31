b4967ff0904fdf1aa04db235d68174f191bb6ada
SOO <ssj060707+github@gmail.com>
2025-11-01 01:30:25 +0800

    prettier lights, fixed switch statement in projectiles

--- src/headers/projectiles.hpp
+++ src/headers/projectiles.hpp
@@ -1,29 +1,28 @@
 #pragma once
 
-#include <unordered_map>
-#include <memory>
-#include <vector>
-
+#include <cstddef>
+#include <array>
 
 #include "assetManager.hpp"
 #include "globalDataWrapper.hpp"
 #include "raylib.h"
 
 
-enum class ProjectileType
+enum class ProjectileType : size_t
 {
 	Bullet,
 	Lazer,
-	Ball
+	Ball,
+	COUNT
 };
 
 class Projectile
 {
 	public:
-		Projectile(float x, float y, Vector2 direction, ProjectileType type, GlobalDataWrapper& global_data, AssetManager& assets) noexcept;
+		Projectile(float x, float y, const Vector2& direction, ProjectileType type, const GlobalDataWrapper& global_data, const AssetManager& assets) noexcept;
 		~Projectile() = default;
 
-		void Update(const Vector2 player_centre) noexcept;
+		void Update(const Vector2& player_centre) noexcept;
 		void Draw() const noexcept;
 		void DrawLightmap() const noexcept;
 
@@ -45,5 +44,39 @@ class Projectile
 
 		int Layer;
 	private:
+		void BulletConstructor(float x, float y, const Vector2& direction, const GlobalDataWrapper& global_data, const AssetManager& assets) noexcept;
+		void LazerConstructor(float x, float y, const Vector2& direction, const GlobalDataWrapper& global_data, const AssetManager& assets) noexcept;
+		void BallConstructor(float x, float y, const Vector2& direction, const GlobalDataWrapper& global_data, const AssetManager& assets) noexcept;
+
+		void BulletUpdate(const Vector2& player_centre) noexcept;
+		void LazerUpdate(const Vector2& player_centre) noexcept;
+		void BallUpdate(const Vector2& player_centre) noexcept;
+		
+		void BulletDrawLightmap() const noexcept;
+		void LazerDrawLightmap() const noexcept;
+		void BallDrawLightmap() const noexcept;
+
+		using ConstructorHook = void(Projectile::*)(float, float, const Vector2&, const GlobalDataWrapper&, const AssetManager&);
+		static inline constexpr std::array<ConstructorHook, static_cast<size_t>(ProjectileType::COUNT)> ConstructorHooks =
+		{
+			&BulletConstructor,
+			&LazerConstructor,
+			&BallConstructor
+		};
+
+		static inline constexpr std::array<void(Projectile::*)(const Vector2&), static_cast<size_t>(ProjectileType::COUNT)> UpdateHooks = 
+		{
+			&BulletUpdate,
+			&LazerUpdate,
+			&BallUpdate
+		};
+
+		static inline constexpr std::array<void(Projectile::*)() const noexcept, static_cast<size_t>(ProjectileType::COUNT)> DrawLightmapHooks =
+		{
+			&BulletDrawLightmap,
+			&LazerDrawLightmap,
+			&BallDrawLightmap
+		};
+
 		Texture2D Image;
 };
--- src/projectiles.cpp
+++ src/projectiles.cpp
@@ -8,7 +8,7 @@
 
 #include <cmath>
 
-Projectile::Projectile(float x, float y, Vector2 direction, ProjectileType type, GlobalDataWrapper& global_data, AssetManager& assets) noexcept :
+Projectile::Projectile(float x, float y, const Vector2& direction, ProjectileType type, const GlobalDataWrapper& global_data, const AssetManager& assets) noexcept :
 	Type(type),
 	Layer(global_data.CurrentLayer) 
 {
@@ -16,35 +16,15 @@ Projectile::Projectile(float x, float y, Vector2 direction, ProjectileType type,
 		this->Direction = Vector2Normalize(direction);
 	else
 		this->Direction = direction;
+
+	auto hook = this->ConstructorHooks[static_cast<size_t>(type)];
+	(this->*hook)(x, y, direction, global_data, assets);
 	
-	switch (this->Type)
-	{
-		case ProjectileType::Bullet:
-			this->Image = assets.Textures.at(TextureKey::Bullet);
-			this->Speed = global_data.Attributes.at(Attribute::BulletSpeed);
-			this->Rotation = atan2(this->Direction.y, this->Direction.x) * TO_DEG;
-			this->Scale = 1.0f;
-
-			break;
-		case ProjectileType::Lazer:
-			this->Image = assets.Textures.at(TextureKey::Lazer);
-			this->Speed = global_data.Attributes.at(Attribute::LazerSpeed);
-			this->Rotation = atan2(this->Direction.y, this->Direction.x) * TO_DEG;
-			this->Scale = global_data.Attributes.at(Attribute::LazerScale);
-
-			break;
-		case ProjectileType::Ball:
-			this->Image = assets.Textures.at(TextureKey::Ball);
-			this->Scale = global_data.Attributes.at(Attribute::BallScale);
-			this->Speed = global_data.Attributes.at(Attribute::BallAngularSpeed);
-			this->Rotation = global_data.Attributes.at(Attribute::BallAngularSpeed) * TO_DEG;
-			this->Radius = global_data.Attributes.at(Attribute::BallRadius);
-
-			break;
-	}
-
 	this->Rect = { x, y, this->Image.width * this->Scale, this->Image.height * this->Scale };
 }
+
+
+
 
 void Projectile::Draw() const noexcept
 {
@@ -54,36 +34,91 @@ void Projectile::Draw() const noexcept
 
 void Projectile::DrawLightmap() const noexcept
 {
-	/*
-	 * AABB is axis aligned, but since this is rotated, there is no way to sync the hitbox and the texture
-	 * as such, the centre is useless and we're better off using the x and y coordinates
-	*/
-	DrawCircleGradient(this->Rect.x, this->Rect.y, std::max(this->Rect.width, this->Rect.height) * 2.0f, WHITE, LIGHTGRAY);
+	auto hook = this->DrawLightmapHooks[static_cast<size_t>(this->Type)];
+	(this->*hook)();
 }
 
-void Projectile::Update(const Vector2 player_centre) noexcept
+void Projectile::Update(const Vector2& player_centre) noexcept
 {
-	switch (this->Type)
-	{
-		case ProjectileType::Ball:
-		{
-			this->CurrentAngle += this->Speed * TICK_TIME;
+	auto hook = this->UpdateHooks[static_cast<size_t>(this->Type)];
+	(this->*hook)(player_centre);
+}
 
-			this->CurrentAngle = std::fmod(this->CurrentAngle, 2 * PI);
 
-			if (this->CurrentAngle < 0.0f)
-				this->CurrentAngle += 2 * PI;
 
-			this->Rect.width = this->Image.width * this->Scale;
-			this->Rect.height = this->Image.height * this->Scale;
+void Projectile::BulletConstructor(float x, float y, const Vector2& direction, const GlobalDataWrapper& global_data, const AssetManager& assets) noexcept
+{
+	this->Image = assets.Textures.at(TextureKey::Bullet);
+	this->Speed = global_data.Attributes.at(Attribute::BulletSpeed);
+	this->Rotation = atan2(this->Direction.y, this->Direction.x) * TO_DEG;
+	this->Scale = 1.0f;
+}
 
-			this->Rect.x = player_centre.x + this->Radius * cos(this->CurrentAngle);
-			this->Rect.y = player_centre.y + this->Radius * sin(this->CurrentAngle);
+void Projectile::LazerConstructor(float x, float y, const Vector2& direction, const GlobalDataWrapper& global_data, const AssetManager& assets) noexcept
+{
+	this->Image = assets.Textures.at(TextureKey::Lazer);
+	this->Speed = global_data.Attributes.at(Attribute::LazerSpeed);
+	this->Rotation = atan2(this->Direction.y, this->Direction.x) * TO_DEG;
+	this->Scale = global_data.Attributes.at(Attribute::LazerScale);
+}
 
-			break;
-		}
-		default:
-			this->Rect.x += this->Direction.x * this->Speed * TICK_TIME;
-			this->Rect.y += this->Direction.y * this->Speed * TICK_TIME;
-	}
+void Projectile::BallConstructor(float x, float y, const Vector2& direction, const GlobalDataWrapper& global_data, const AssetManager& assets) noexcept
+{
+	this->Image = assets.Textures.at(TextureKey::Ball);
+	this->Scale = global_data.Attributes.at(Attribute::BallScale);
+	this->Speed = global_data.Attributes.at(Attribute::BallAngularSpeed);
+	this->Rotation = 0.0f; //global_data.Attributes.at(Attribute::BallAngularSpeed) * TO_DEG;
+	this->Radius = global_data.Attributes.at(Attribute::BallRadius);
+}
+
+
+
+/*
+ * since AABB is axis aligned by definition, there is no way to sync the hitbox and the texture if rotated.
+ * as such, calculating the centre is useless and we're better off using the top left x and y coordinates
+*/
+void Projectile::BulletDrawLightmap() const noexcept
+{
+	DrawCircleGradient(this->Rect.x, this->Rect.y, std::max(this->Rect.width, this->Rect.height) * 2.0f, YELLOW, LIGHTGRAY);
+}
+
+void Projectile::LazerDrawLightmap() const noexcept
+{
+	DrawCircleGradient(this->Rect.x, this->Rect.y, std::max(this->Rect.width, this->Rect.height) * 2.0f, CYAN, LIGHTGRAY);
+}
+
+void Projectile::BallDrawLightmap() const noexcept
+{
+	DrawCircleGradient(this->Rect.x, this->Rect.y, std::max(this->Rect.width, this->Rect.height) * 2.0f, CYAN, LIGHTGRAY);
+}
+
+
+
+void Projectile::BulletUpdate(const Vector2& player_centre) noexcept
+{
+	this->Rect.x += this->Direction.x * this->Speed * TICK_TIME;
+	this->Rect.y += this->Direction.y * this->Speed * TICK_TIME;
+}
+
+void Projectile::LazerUpdate(const Vector2& player_centre) noexcept
+{
+
+	this->Rect.x += this->Direction.x * this->Speed * TICK_TIME;
+	this->Rect.y += this->Direction.y * this->Speed * TICK_TIME;
+}
+
+void Projectile::BallUpdate(const Vector2& player_centre) noexcept
+{
+	this->CurrentAngle += this->Speed * TICK_TIME;
+
+	this->CurrentAngle = std::fmod(this->CurrentAngle, 2 * PI);
+
+	if (this->CurrentAngle < 0.0f)
+		this->CurrentAngle += 2 * PI;
+
+	this->Rect.width = this->Image.width * this->Scale;
+	this->Rect.height = this->Image.height * this->Scale;
+
+	this->Rect.x = player_centre.x + this->Radius * cos(this->CurrentAngle);
+	this->Rect.y = player_centre.y + this->Radius * sin(this->CurrentAngle);
 }
